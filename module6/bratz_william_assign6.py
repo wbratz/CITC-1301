3c6bcd7fd07099384a3c6df017cc266b727c40a4
Billy <williambratz00@gmail.com>
2021-06-12 11:33:01 -0500

    Module 6 assignment and lab - Complete

---
+++ module6/bratz_william_assign6.py
@@ -0,0 +1,17 @@
+print("How many days did you collect gems?")
+
+days_collected = int(input())
+total_gems = 0
+day_number = 1
+
+while(day_number <= days_collected):
+    print("Enter the number of gems collected on day:", day_number)
+    total_gems += int(input())
+    day_number += 1
+
+print("Total gems collected:", total_gems)
+print("Average gems collected per day:", total_gems / days_collected)
+
+
+
+
