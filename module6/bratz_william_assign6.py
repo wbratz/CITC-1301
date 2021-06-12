92dbffe528507bbaa93b68c4d3c7ec0ba5c447d0
Billy <williambratz00@gmail.com>
2021-06-12 11:54:02 -0500

    being extra

--- module6/bratz_william_assign6.py
+++ module6/bratz_william_assign6.py
@@ -12,6 +12,20 @@ while(day_number <= days_collected):
 print("Total gems collected:", total_gems)
 print("Average gems collected per day:", total_gems / days_collected)
 
+## not sure on this assignment if you wanted to see it terminated to two decimal places (like in the sample output)
+## or if you wanted just the full average, the instructions didn't explicitly say, so I did it both ways 
+## I went with the the above actually running because it offered the greatest amount of precision
+
+## below is a couple ways to do it just based on how you wanted the output
+
+## Rounding up which is technically more accurate then the sample output in the assignment
+# print("Average gems collected per day:", round(total_gems / days_collected, 2))
+
+## Matching the sample output exactly
+# print("Average gems collected per day:", float(str(total_gems / days_collected)[:4]))
+
+
+
 
 
 
