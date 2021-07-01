a63310430b52d6c8e9840265de4092ff1684637e
Billy <williambratz00@gmail.com>
2021-06-30 19:30:46 -0500

    updates

--- module9/bratz_william_assign9.py
+++ module9/bratz_william_assign9.py
@@ -1,84 +1,50 @@
-# Exercise
-# Write a program that converts the common measurements below.  Each of the conversion functions should return the result.
 
-# Each conversion must happen inside of a function that you make for a total of 4 conversion functions.
-# The conversion functions must not print the result.
-
-# The conversion functions must accept the value to convert as an input parameter.
-
-# Do not define functions inside "if" statements, loops, or branching/conditional logic. 
-# Function: Convert inches to feet . (12 inches = 1 foot)
-
-# Function: Convert feet to yards(3 feet = 1 yard)
-
-# Function: Convert miles to yards. (1 mile = 1760 yards)
-
-# Function: Convert miles to feet. (1 mile = 5280 feet)
-
-# After calling the function, and after the function returns the value, print the conversion to the screen.
-
-# Display a menu that asks the user to select from the above options and with the option to exit the program.
-
-# After the user selects a menu option and the output is printed.
-# Sample input/output:
-# Conversion Menu:
-
-# 1. Convert feet to inches
-# 2. Convert yards to feet
-# 3. Convert miles to yards
-# 4. Convert miles to feet
-# 5. Exit
-
-# Please choose a menu option: 1
-# Enter the number of feet: 3
-
-# There are 36 inches in 3 feet.
-
-# Goodbye.
 
 def main():
-    print_menu()
 
-    user_selection = get_menu_option()
+    user_selection = 0
 
-    # If user does not select exit
-    if user_selection != 5:
-        #set initial values
-        init_value = 0
-        calc_value = 0
-        from_value = ""
-        to_value = ""
+    while user_selection != 5:
+        print_menu()
+        # If user does not select exit
+        user_selection = get_menu_option()
+        if user_selection != 5:
+            #set initial values
+            init_value = 0
+            calc_value = 0
+            from_value = ""
+            to_value = ""
 
-        # feet to yards
-        if user_selection == 1:
-            from_value = "feet"
-            to_value = "inches"
-            init_value = get_initial_value(from_value)
-            calc_value = feet_to_inches(init_value)
+            # feet to yards
+            if user_selection == 1:
+                from_value = "feet"
+                to_value = "inches"
+                init_value = get_initial_value(from_value)
+                calc_value = feet_to_inches(init_value)
 
-        # yards to feet
-        elif user_selection == 2:
-            from_value = "yards"
-            to_value = "feet"
-            init_value = get_initial_value(from_value)
-            calc_value = yards_to_feet(init_value)
-
-        # handle getting the value for both mile conversions
-        elif user_selection == 3 or user_selection == 4:
-            from_value = "miles"
-            init_value = get_initial_value(from_value)            
-
-            # miles to yards
-            if user_selection == 3:
-                to_value = "yards"
-                calc_value = miles_to_yards(init_value)
-
-            #miles to feet
-            if user_selection == 4:
+            # yards to feet
+            elif user_selection == 2:
+                from_value = "yards"
                 to_value = "feet"
-                calc_value = miles_to_feet(init_value)
+                init_value = get_initial_value(from_value)
+                calc_value = yards_to_feet(init_value)
 
-        print_calc(to_value, calc_value, from_value, init_value)
+            # handle getting the value for both mile conversions
+            elif user_selection == 3 or user_selection == 4:
+                from_value = "miles"
+                init_value = get_initial_value(from_value)            
+
+                # miles to yards
+                if user_selection == 3:
+                    to_value = "yards"
+                    calc_value = miles_to_yards(init_value)
+
+                #miles to feet
+                if user_selection == 4:
+                    to_value = "feet"
+                    calc_value = miles_to_feet(init_value)
+
+            print_calc(to_value, calc_value, from_value, init_value)
 
     print_exit()
 
