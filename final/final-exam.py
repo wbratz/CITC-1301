f9ae2b2ed4f38424d53185a642d03b32a75bac00
Billy <Billy@williambratz.com>
2021-07-31 04:17:14 -0500

    Final exam code, and programming assignment

---
+++ final/final-exam.py
@@ -0,0 +1,52 @@
+# for num in range(0, 20, 5):
+#     num += num
+# print(num) 
+
+# 30
+
+# student = 1
+
+# while student <=3:
+#     total = 0
+#     for score in range(1, 4):
+#         score = int(input("Enter test score: "))
+#         total += score
+
+#     average = total/3
+#     print("Student ", student, "average: ", average)
+#     student +=1
+
+
+# print('I\'m ready to begin')
+
+# for num in range(2, 9, 2):
+#     print(num)
+
+# Make a list with at least three of your favorite colors.
+# Write a function called list_writer that accepts a list as a parameter. 
+# Inside the list_writer function, read the list item by item. 
+# Inside the list_writer function, save each item in the list to a text file called colors.txt (put one color per line in the text file).
+# When you call your function, pass the list into the function as a parameter/argument.
+
+# fav_colors = ["blue", "red", "gray"]
+
+# def list_writer(list):
+#     file = open("colors.txt", "w")
+
+#     for item in list:
+#         file.write(item + "\n")
+
+
+# list_writer(fav_colors)
+
+# number = range(0, 9, 2)
+# for num in number:
+#     print(num)
+
+
+# total = 0
+# for count in range(1,4):
+#      total += count
+# print(total)
+
+print(2.0 * 12)
